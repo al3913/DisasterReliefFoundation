6eddd15c554474392fc3df0b68e4a0fa5375f286
jap2739 <jap2739@rit.edu>
2024-02-17 11:07:28 -0500

    -a

---
+++ ufund-api/src/main/java/com/Need.java
@@ -0,0 +1,18 @@
+package com;
+
+public class Need {
+    private String name;
+    private double cost;
+    private int quantity; 
+    private String type;  
+
+    public Need(String name, double cost, int quantity, String type){
+        this.name = name;
+        this.cost = cost;
+        this.quantity = quantity;
+        this.type = type;
+    }
+
+    
+
+}
