a44531a5909794bab9c588894c8b43f95cb12f7e
jap2739 <jap2739@rit.edu>
2024-02-19 19:22:36 -0500

    trying to run commands

--- ufund-api/src/main/java/com/ufund/api/model/Need.java
+++ ufund-api/src/main/java/com/ufund/api/model/Need.java
@@ -1,35 +1,60 @@
 package com.ufund.api.model;
 
+import java.util.logging.Logger;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+/**
+ * Represents a Hero entity
+ * 
+ * @author SWEN Faculty
+ */
 public class Need {
-    private String name;
-    private double cost;
-    private int quantity; 
-    private String type;  
-    private int id;
+    private static final Logger LOG = Logger.getLogger(Need.class.getName());
 
-    public Need(String name, double cost, int quantity, String type, int id){
-        this.name = name;
-        this.cost = cost;
-        this.quantity = quantity;
-        this.type = type;
+    // Package private for tests
+    static final String STRING_FORMAT = "Need [id=%d, name=%s]";
+
+    @JsonProperty("id") private int id;
+    @JsonProperty("name") private String name;
+
+    /**
+     * Create a hero with the given id and name
+     * @param id The id of the hero
+     * @param name The name of the hero
+     * 
+     * {@literal @}JsonProperty is used in serialization and deserialization
+     * of the JSON object to the Java object in mapping the fields.  If a field
+     * is not provided in the JSON object, the Java field gets the default Java
+     * value, i.e. 0 for int
+     */
+    public Need(@JsonProperty("id") int id, @JsonProperty("name") String name) {
         this.id = id;
+        this.name = name;
     }
 
-    public String getName(){
-        return this.name;
-    }
-    public double getCost(){
-        return this.cost;
-    }
-    public int getQuantity(){
-        return this.quantity;
-    }
-    public String getType(){
-        return this.type;
-    }
+    /**
+     * Retrieves the id of the hero
+     * @return The id of the hero
+     */
+    public int getId() {return id;}
 
-    public int getId(){
-        return this.id;
-    }
+    /**
+     * Sets the name of the hero - necessary for JSON object to Java object deserialization
+     * @param name The name of the hero
+     */
+    public void setName(String name) {this.name = name;}
 
+    /**
+     * Retrieves the name of the hero
+     * @return The name of the hero
+     */
+    public String getName() {return name;}
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return String.format(STRING_FORMAT,id,name);
+    }
 }
