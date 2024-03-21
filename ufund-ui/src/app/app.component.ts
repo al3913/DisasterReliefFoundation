edea33b37f5678905550532d6cb4be5af34872a7
jap2739 <jap2739@rit.edu>
2024-03-20 20:20:23 -0400

    WORKING FRONT END (Kinda)

--- ufund-ui/src/app/app.component.ts
+++ ufund-ui/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Needs';
+
+  logout(){
+    window.location.href="http://localhost:4200/login";
+  }
+
 }
